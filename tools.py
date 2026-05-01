0cd4ed2fdec614f6da924670a124ad3a02f13115
sachinsrivastavadatascientist-lab <sachinsrivastavadatascientist@gmail.com>
2026-05-01 19:25:52 +0530

    u

--- tools.py
+++ tools.py
@@ -33,7 +33,7 @@ def yt_tool(YT_URL: str) -> str:
         transcript =transcript[:20]
 
         text= " ".join([t.text for t in transcript])
-        return  text[:800]
+        return  text[:500]
 
     except Exception as e:
         return f"Error fetching transcript: {str(e)}"
